80dc552c360ca760bbb7568061b2361b3b393c78
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-07-24 15:59:48 -0400

    create favoritesSlice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipesSlice, type RecipesSliceType } from './recipeSlice'
-
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+import { type FavoritesSliceType, createFavoritesSlice } from './favoritesSlice'
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
 })))
